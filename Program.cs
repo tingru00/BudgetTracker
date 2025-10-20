278c181ca3002975a6c9adeaf65251459f4c7249
tingru00 <grundbergtindra@gmail.com>
2025-10-20 13:12:47 +0200

    Gjort en meny i program.cs, ändrat kommentar i klass Transaction som var från när jag ändrade från privata attribut i Transaction.cs till istället ha en privat lista i BudgetManager.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace BudgetTracker
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BudgetManager manager = new BudgetManager();
+
+            bool running = true;
+            while(running)
+            {
+                //Skapar en meny
+                Console.WriteLine("Välkommen till ditt BudgetTracker konto!");
+                Console.ReadLine();
+                Console.WriteLine("Vad vill du göra? Ange ett val nedan:");
+                Console.WriteLine("1: Lägg till transaktion");
+                Console.WriteLine("2: Visa alla transaktioner");
+                Console.WriteLine("3: Visa total balans");
+                Console.WriteLine("4: Ta bort transaktion");
+                Console.WriteLine("5: Avsluta");
+            }
+            
         }
     }
 }
